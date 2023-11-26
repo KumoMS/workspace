8833c45b0ce2db7f75de43ef1179c98a7638d458
Marti Stephens <mkumostephens@gmail.com>
2023-11-26 03:03:37 +0000

    else elif commit 2

--- Williamfiset/4.3.else.elif.py
+++ Williamfiset/4.3.else.elif.py
@@ -1,32 +1,45 @@
 # Else and elif operators
-
+import sys 
 #Name is Kuro so all following if and elifs will print the else statements
 #If I change Kuro to Meli, Skylar, Kumo or Shodan it will return the name to me
-name = "Kumo"
+
+#when i add
+#print("please enter name: ")
+#it fails to recognise the name input correctly for some reason
+name = str(input())
+
 if name == "Meli":
     print("Name is Meli")
+    sys.exit()
 else:
     print("Name was not Meli")
-
+    
 #Elif puts all the if statements together
 if name == "Skylar":
     print("Name is Skylar")
+    
 elif name == "Kumo":
     print("name is Kumo".capitalize())
+    
 elif name == "Shodan":
     print("Shodan")
+    
 else:
     print("not Skylar, not Kumo, and not Shodan")
+    
 
 #without the elif these if statements become independant
 if name == "Skylar":
     print("Name is Skylar")
+    
 
 if name == "Kumo":
     print("Name is Kumo")
+    
 
 if name == "Shodan":
     print("Name is Shodan")
+    sys.exit()
 
 #using numbers - changing the number according to the if and elif statements will return the respected statement
 n = 70
