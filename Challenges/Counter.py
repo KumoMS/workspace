1d3f5488365b76b3087699c2a190e46d5ef15155
Marti Stephens <mkumostephens@gmail.com>
2023-12-05 01:11:33 +0000

    im trytingh here

--- Challenges/Counter.py
+++ Challenges/Counter.py
@@ -6,6 +6,11 @@ count = 1
 while count <= 5:
     print(count)
     count = count+1
+
+
+
+
+
 #while count >= 5:
  #   count = count-5
     
---
+++ Challenges/Counter2.py
@@ -0,0 +1,10 @@
+I = input("Please hit the Enter key")
+c = 1
+while c <=5:
+
+    print(c)
+    c = c+1
+
+while c == 5:
+    print(c)
+    c = c-5
