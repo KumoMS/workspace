28e23cb2bba8ba79538acea159d1277bfb87a54a
Marti Stephens <mkumostephens@gmail.com>
2023-12-11 07:07:27 +0000

    commit 11/12

---
+++ _tools/clearcmd.py
@@ -0,0 +1,7 @@
+import os
+clear = lambda: os.system('cls')
+clear()
+
+# Typing this into the cmd line allows me to use clear() in order to clear screen
+# take note how this makes use of the import os feature
+# lets me use the cls function from cmd line in the python script 
---
+++ _tools/dictionary.py
@@ -0,0 +1,5 @@
+# Dictionaries are similar to lists but instead of [] we use {}
+# and the way that they work are instead of indexing they use keys as shown below
+dict_1 = {"0":"Kuro", "1":"Kumo", "A":"Kurai", "B":"Kaede"}
+print(dict_1)
+print(dict_1["A"])
